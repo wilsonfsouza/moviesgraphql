3a10be3401995239991070c2e44bc4a509d62a69
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-26 22:17:52 -0700

    feat: added function to create a category that receives a categoryInput as argument.

--- src/graphql/category/CategoryResolver.ts
+++ src/graphql/category/CategoryResolver.ts
@@ -1,6 +1,15 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
 import Category from "./Category";
 import CategoriesSchema from "../../models/CategoriesSchema";
+
+@InputType()
+class CategoryInput {
+    @Field()
+    description: String;
+
+    @Field()
+    name: String;
+}
 
 @Resolver(Category)
 class CategoryResolver {
@@ -10,6 +19,12 @@ class CategoryResolver {
         const categories = await CategoriesSchema.find();
 
         return categories;
+    }
+
+    @Mutation(() => Category)
+    async createCategory(@Arg("categoryInput") categoryInput: CategoryInput) {
+        const category = await CategoriesSchema.create(categoryInput);
+        return category;
     }
 }
 
