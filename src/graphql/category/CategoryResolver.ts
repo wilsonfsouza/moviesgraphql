98c3aeeb7adecf28df9a82b5b9b9f57fb37d4e97
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-26 21:48:10 -0700

    feat: used categoriesSchema to search for categories in db.

--- src/graphql/category/CategoryResolver.ts
+++ src/graphql/category/CategoryResolver.ts
@@ -1,12 +1,15 @@
 import { Query, Resolver } from "type-graphql";
 import Category from "./Category";
+import CategoriesSchema from "../../models/CategoriesSchema";
 
 @Resolver(Category)
 class CategoryResolver {
 
     @Query(() => [Category])
     async categories() {
+        const categories = await CategoriesSchema.find();
 
+        return categories;
     }
 }
 
